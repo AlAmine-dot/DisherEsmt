cbae39ed9911e7ac559d8d99a00bbbd254a6298e
bassirou Loum <bassirou825@gmail.com>
2023-04-24 16:33:29 +0000

    onbarding 2

--- app/src/main/java/com/example/esmt/cours/disher/core/presentation/onboarding/OnBoardingContent.kt
+++ app/src/main/java/com/example/esmt/cours/disher/core/presentation/onboarding/OnBoardingContent.kt
@@ -1,20 +1,25 @@
 package com.example.esmt.cours.disher.core.presentation.onboarding
 
-import android.util.Log
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExperimentalAnimationApi
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.BottomAppBar
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,89 +29,150 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.example.esmt.cours.disher.R
 import com.example.esmt.cours.disher.core.presentation.graphs.Graph
 import com.example.esmt.cours.disher.core.util.OnBoardingPage
+import com.example.esmt.cours.disher.ui.theme.MeltyGreen
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.HorizontalPagerIndicator
-import com.google.accompanist.pager.PagerState
 import com.google.accompanist.pager.rememberPagerState
+import androidx.compose.animation.core.animateDpAsState
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 //@ExperimentalAnimationApi
 @ExperimentalPagerApi
 @Composable
 fun OnBoardingContent(
-    navController: NavHostController,
-    welcomeViewModel: OnBoardingViewModel = hiltViewModel()
-) {
-    val pages = listOf(
+    pages: List<OnBoardingPage> = listOf(
         OnBoardingPage.First,
         OnBoardingPage.Second,
-        OnBoardingPage.Third
-    )
+        OnBoardingPage.Third),
+    navController: NavHostController,
+    welcomeViewModel: OnBoardingViewModel = hiltViewModel(),
+    applicationName: String = "Disher",
+) {
     val pagerState = rememberPagerState()
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        HorizontalPager(
-            modifier = Modifier.weight(10f),
-            count = 3,
-            state = pagerState,
-            verticalAlignment = Alignment.Top
-        ) { position ->
-            PagerScreen(onBoardingPage = pages[position])
-        }
-        HorizontalPagerIndicator(
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .weight(1f),
-            pagerState = pagerState
-        )
-        FinishButton(
-            modifier = Modifier.weight(1f),
-            pagerState = pagerState
-        ) {
-            welcomeViewModel.saveOnBoardingState(completed = true)
-            navController.popBackStack()
-            navController.navigate(Graph.HOME.route)
-            Log.d("testonb",welcomeViewModel.toString())
-        }
-    }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                elevation = 16.dp,
+                backgroundColor = Color.White,
+                content = {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        contentAlignment = Alignment.Center
+                    ) {
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .align(Alignment.Center),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                painterResource(id = R.drawable.ic_disher_white),
+                                contentDescription = "",
+                                tint = Color.Unspecified,
+                                modifier = Modifier.size(60.dp)
+                            )
+                            Text(
+                                text = applicationName,
+                                color = MeltyGreen,
+                                style = MaterialTheme.typography.h3,
+                                fontWeight = FontWeight.SemiBold,
+                                fontSize = 30.sp
+                            )
+                        }
+
+                    }
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                // elevation = 16.dp,
+                backgroundColor = Color.White,
+                modifier = Modifier
+                    .height(80.dp)
+                    .fillMaxWidth(),
+                content = {
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        if (pagerState.currentPage == pages.lastIndex) {
+                            FinishButton(
+                                modifier = Modifier
+                                    .align(Alignment.CenterHorizontally)
+                                    .weight(1f),
+                                //pagerState = viewPagerState
+                            ) {
+                                welcomeViewModel.saveOnBoardingState(completed = true)
+                                navController.popBackStack()
+                                navController.navigate(Graph.HOME.route)
+                            }
+                        }
+                        HorizontalPagerIndicator(
+                            activeColor = MeltyGreen,
+                            pagerState = pagerState,
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .weight(1f),
+                        )
+                    }
+                }
+            )
+        },
+        content = {
+            Column(modifier = Modifier.fillMaxSize()) {
+                HorizontalPager(state = pagerState, count = pages.size) { page ->
+                    PagerScreen(onBoardingPage = pages[page])
+                }
+            }
+        },
+    )
 }
 
 @Composable
 fun PagerScreen(onBoardingPage: OnBoardingPage) {
     Column(
-        modifier = Modifier
-            .fillMaxWidth(),
+        modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top
     ) {
-        Image(
-            modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .fillMaxHeight(0.7f),
-            painter = painterResource(id = onBoardingPage.image),
-            contentDescription = "Pager Image"
-        )
         Text(
-            modifier = Modifier
-                .fillMaxWidth(),
             text = onBoardingPage.title,
-            fontSize = MaterialTheme.typography.h4.fontSize,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 40.dp),
+            style = MaterialTheme.typography.h5,
             fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center
         )
+
         Text(
+            text = onBoardingPage.description,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 30.dp)
-                .padding(top = 15.dp),
-            text = onBoardingPage.description,
+                .padding(horizontal = 40.dp)
+                .padding(top = 20.dp),
+            style = MaterialTheme.typography.h6,
             fontSize = MaterialTheme.typography.subtitle1.fontSize,
             fontWeight = FontWeight.Medium,
             textAlign = TextAlign.Center
+        )
+
+        Image(
+            painter = painterResource(id = onBoardingPage.image),
+            contentDescription = onBoardingPage.title,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.7f),
         )
     }
 }
@@ -116,7 +182,6 @@ fun PagerScreen(onBoardingPage: OnBoardingPage) {
 @Composable
 fun FinishButton(
     modifier: Modifier,
-    pagerState: PagerState,
     onClick: () -> Unit
 ) {
     Row(
@@ -125,18 +190,11 @@ fun FinishButton(
         verticalAlignment = Alignment.Top,
         horizontalArrangement = Arrangement.Center
     ) {
-        AnimatedVisibility(
-            modifier = Modifier.fillMaxWidth(),
-            visible = pagerState.currentPage == 2
+        Button(
+            onClick = onClick,
+            colors = ButtonDefaults.buttonColors(MeltyGreen)
         ) {
-            Button(
-                onClick = onClick,
-                colors = ButtonDefaults.buttonColors(
-                    contentColor = Color.White
-                )
-            ) {
-                Text(text = "Finish", color = Color.White)
-            }
+            Text(text = "Finish", color = Color.White)
         }
     }
 }
--- app/src/main/java/com/example/esmt/cours/disher/core/util/OnBoardingPage.kt
+++ app/src/main/java/com/example/esmt/cours/disher/core/util/OnBoardingPage.kt
@@ -1,29 +1,95 @@
 package com.example.esmt.cours.disher.core.util
 
 import androidx.annotation.DrawableRes
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import com.example.esmt.cours.disher.R
+import com.example.esmt.cours.disher.ui.theme.DarkTurquoise
+import com.example.esmt.cours.disher.ui.theme.MeltyGreen
 
 sealed class OnBoardingPage(
     @DrawableRes
     val image: Int,
     val title: String,
-    val description: String
+    val description: AnnotatedString
 ) {
     object First : OnBoardingPage(
         image = R.drawable.image1,
         title = "Loads of ideas",
-        description = "Welcome, fellow disher ! Find what you're looking for with over 76,000 recipes to satisfy any craving (and your fridge's too)."
+        description = buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    color = DarkTurquoise,
+                )
+            ) {
+                append("Welcome, fellow disher ! Find what you're looking ")
+            }
+            withStyle(
+                style = SpanStyle(
+                    color = MeltyGreen,
+                )
+            ) {
+                append("for with over 76,000 ")
+            }
+            withStyle(
+                style = SpanStyle(
+                    color = DarkTurquoise,
+                )
+            ) {
+                append("recipes to satisfy any craving (and your fridge's too).")
+            }
+        }
     )
 
     object Second : OnBoardingPage(
         image = R.drawable.image2,
         title = "Discovery",
-        description = "Here, there is something for everyone! With thousands of recipes for all tastes and skill levels, you are sure to find something that suits you."
+        description = buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    color = DarkTurquoise,
+                )
+            ) {
+                append("Here, there is something for everyone! With thousands of recipes for all tastes and skill levels, ")
+            }
+            withStyle(
+                style = SpanStyle(
+                    color = MeltyGreen,
+                )
+            ) {
+                append("you are sure to find something that suits you.")
+            }
+        }
     )
 
     object Third : OnBoardingPage(
         image = R.drawable.image3,
         title = "Learn",
-        description = "Learn new cooking skills with Disher ! We're here to help you improve your cooking skills and discover new ingredients."
+        description = buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    color = DarkTurquoise,
+                )
+            ) {
+                append("Learn new cooking skills with Disher ! We're here to help you improve your ")
+            }
+            withStyle(
+                style = SpanStyle(
+                    color = MeltyGreen,
+                )
+            ) {
+                append("cooking skills ")
+            }
+            withStyle(
+                style = SpanStyle(
+                    color = DarkTurquoise,
+                )
+            ) {
+                append("and discover new ingredients.")
+            }
+        }
+
     )
 }
